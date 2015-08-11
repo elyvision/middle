099e8b58e06e63d33b544bdceea925e1d6622247
Elie Marot <elie.marot@gmail.com>
2015-08-11 15:33:29 +0200

    exo4

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,34 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $("#map").scroll(function(e){
+    if ($(this).scrollTop() > 50) {
+      $(".navbar").css({
+        "margin-top": "-70px"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "margin-top": "0px"
+      });
+    }
+  });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+});
